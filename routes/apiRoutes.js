30bd130280d314bc4b0b09f8562816edc8ab00af
benohhh <b3njaminoh@gmail.com>
2018-12-11 17:42:30 -0800

    deleting before saving db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,25 +6,26 @@ const db = require('../models')
 module.exports = (app) => {
 
   app.get('/scrape', (req, res) => {
-    axios.get('https://www.nytimes.com/section/world/')
-      .then(r => {
-        const $ = cheerio.load(r.data)
-        let articlesArr = []
-        $('div.story-body').each((i, elem) => {
-          if (i <= 9) {
-          articlesArr.push({
-               headline: $(elem).children('h2.headline').text().trim(),
-               summary: $(elem).children('p.summary').text(),
-               url: $(elem).children('h2.headline').children('a').attr('href')
+    db.Article.delete({}, (e, d) => {
+      axios.get('https://www.nytimes.com/section/world/')
+        .then(r => {
+          const $ = cheerio.load(r.data)
+          let articlesArr = []
+          $('div.story-body').each((i, elem) => {
+            if (i <= 9) {
+              articlesArr.push({
+                headline: $(elem).children('h2.headline').text().trim(),
+                summary: $(elem).children('p.summary').text(),
+                url: $(elem).children('h2.headline').children('a').attr('href')
               })
             }
+          })
+          db.Article.create(articlesArr, (e, d) => {
+            if (e) console.log(e)
+          })
+          res.send(articlesArr)
         })
-        db.Article.create(articlesArr, (e, d) => {
-          if (e) console.log(e)
-        })
-        res.send(articlesArr)
-      })
-      .catch(e => console.log(e))
+        .catch(e => console.log(e))
   })
   
   app.get('/articles', (req, res) => {
